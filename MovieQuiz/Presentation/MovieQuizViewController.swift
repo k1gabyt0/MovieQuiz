cc7aaa1eda428bbb46549979cc78518c812dc55b
Andrei Kigai <avkigai@gmail.com>
2024-12-08 13:30:42 +0300

    Вынес получение вопросов в фабрику

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,8 +1,6 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
-    private var questions: Questions = Questions(all: mockQuestions)
-    
     @IBOutlet private weak var counterLabel: UILabel!
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var questionLabel: UILabel!
@@ -10,20 +8,45 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet private weak var noButton: UIButton!
     @IBOutlet private weak var yesButton: UIButton!
     
+    private var correctAnswersCount: Int = 0
+    private var currentQuestionIndex: Int = 0
+    private let questionsAmount: Int = 10
+    private var questionFactory: QuestionFactoryProtocol = QuestionFactory()
+    private var currentQuestion: QuizQuestion?
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        show(quiz: convert(model: questions.current))
+        if let firstQuestion = questionFactory.requestNextQuestion() {
+            currentQuestion = firstQuestion
+            show(quiz: convert(model: firstQuestion))
+        }
     }
     
     @IBAction private func noButtonClicked(_ sender: Any) {
-        let isCorrect = questions.checkCurrentQuestion(answer: false)
+        guard let currentQuestion = currentQuestion else {
+            return
+        }
+        
+        let isCorrect = currentQuestion.check(answer: false)
+        if isCorrect {
+            correctAnswersCount += 1
+        }
+        
         showAnswerResult(isCorrect: isCorrect)
         noButton.isEnabled = false // дисейблим чтобы нельзя было нажать 100 раз
     }
     
     @IBAction private func yesButtonClicked(_ sender: Any) {
-        let isCorrect = questions.checkCurrentQuestion(answer: true)
+        guard let currentQuestion = currentQuestion else {
+            return
+        }
+        
+        let isCorrect = currentQuestion.check(answer: true)
+        if isCorrect {
+            correctAnswersCount += 1
+        }
+        
         showAnswerResult(isCorrect: isCorrect)
         yesButton.isEnabled = false // дисейблим чтобы нельзя было нажать 100 раз
     }
@@ -44,11 +67,16 @@ final class MovieQuizViewController: UIViewController {
             preferredStyle: .alert
         )
         
-        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
-            self.questions.reset()
+        let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
+            guard let self = self else { return }
             
-            let viewModel = self.convert(model: self.questions.current)
-            self.show(quiz: viewModel)
+            correctAnswersCount = 0
+            
+            if let firstQuestion = questionFactory.requestNextQuestion() {
+                currentQuestionIndex = 0
+                currentQuestion = firstQuestion
+                show(quiz: convert(model: firstQuestion))
+            }
         }
         
         alert.addAction(action)
@@ -68,16 +96,20 @@ final class MovieQuizViewController: UIViewController {
     }
     
     private func showNextQuestionOrResults() {
-        let hasNextQuestion = questions.next()
-        if !hasNextQuestion {
+        if currentQuestionIndex == questionsAmount-1 {
             let result = QuizResultViewModel(
                 title: "Этот раунд окончен!",
-                text: "Ваш результат: \(questions.correctAnswers)/\(questions.count)",
+                text: "Ваш результат: \(correctAnswersCount)/\(questionsAmount)",
                 buttonText: "Сыграть ещё раз"
             )
             show(quiz: result)
-        } else {
-            show(quiz: convert(model: questions.current))
+            return
+        }
+        
+        if let nextQuestion = questionFactory.requestNextQuestion() {
+            currentQuestion = nextQuestion
+            currentQuestionIndex += 1
+            show(quiz: convert(model: nextQuestion))
         }
     }
     
@@ -85,7 +117,7 @@ final class MovieQuizViewController: UIViewController {
         return QuizStepViewModel(
             image: UIImage(named: model.image) ?? getDefaultImage(),
             question: model.text,
-            questionNumber: "\(questions.currentIdx + 1)/\(questions.count)"
+            questionNumber: "\(currentQuestionIndex + 1)/\(questionsAmount)"
         )
     }
     
@@ -99,133 +131,3 @@ final class MovieQuizViewController: UIViewController {
         UIImage(named: "Default") ?? UIImage()
     }
 }
-
-private struct QuizQuestion {
-    let image: String
-    let text: String
-    let correctAnswer: Bool
-    
-    func check(answer: Bool) -> Bool {
-        return answer == correctAnswer
-    }
-}
-
-private struct QuizStepViewModel {
-    let image: UIImage
-    let question: String
-    let questionNumber: String
-}
-
-private struct QuizResultViewModel {
-    let title: String
-    let text: String
-    let buttonText: String
-}
-
-private struct Questions {
-    private let all: [QuizQuestion]
-    private var _currentIdx: Int = 0
-    private var _correctAnswers: Int = 0
-    
-    var current: QuizQuestion {
-        all[currentIdx]
-    }
-    var currentIdx: Int {
-        get {
-            _currentIdx
-        }
-        set {
-            _currentIdx = newValue
-        }
-    }
-    var count: Int {
-        all.count
-    }
-    
-    var correctAnswers: Int {
-        set {
-            _correctAnswers = newValue
-        }
-        get {
-            _correctAnswers
-        }
-    }
-    
-    init(all: [QuizQuestion]) {
-        self.all = all
-    }
-    
-    mutating func checkCurrentQuestion(answer: Bool) -> Bool {
-        let isCorrect = current.check(answer: answer)
-        if isCorrect {
-            correctAnswers += 1
-        }
-        return isCorrect
-    }
-    
-    mutating func next() -> Bool {
-        if currentIdx >= all.count - 1 {
-            return false
-        }
-        currentIdx += 1
-        return true
-    }
-    
-    mutating func reset() {
-        currentIdx = 0
-        correctAnswers = 0
-    }
-}
-
-private var mockQuestions: [QuizQuestion] = [
-    QuizQuestion(
-        image: "The Godfather",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: true
-    ),
-    QuizQuestion(
-        image: "The Dark Knight",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: true
-    ),
-    QuizQuestion(
-        image: "Kill Bill",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: true
-    ),
-    QuizQuestion(
-        image: "The Avengers",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: true
-    ),
-    QuizQuestion(
-        image: "Deadpool",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: true
-    ),
-    QuizQuestion(
-        image: "The Green Knight",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: true
-    ),
-    QuizQuestion(
-        image: "Old",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: false
-    ),
-    QuizQuestion(
-        image: "The Ice Age Adventures of Buck Wild",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: false
-    ),
-    QuizQuestion(
-        image: "Tesla",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: false
-    ),
-    QuizQuestion(
-        image: "Vivarium",
-        text: "Рейтинг этого фильма больше чем 6?",
-        correctAnswer: false
-    )
-]
